744fd23edd4aec338e4f7704129cd96a37d814a1
Don <don@mint.com>
2023-09-24 12:24:27 -0600

    added the exit code to the error

--- my_unittest.py
+++ my_unittest.py
@@ -1,9 +1,14 @@
 from rectangle import Rectangle
 
 def assert_equal(*args):
-    assert expresion == response
+    try:
+        assert expresion == response
+        print('Test passed!')
+    except AssertionError:
+        print('Test Failed :(')
+        exit(1)
 
 rectangle = Rectangle(3,3)
 expresion = rectangle.get_area()
-response = 9
+response = 10
 assert_equal(expresion, response)
